20553c8d70620653d40d02b7d7c391241c2668b1
Iakov Mishchenko <iakov@kamilabs.com>
2018-05-30 17:37:06 +0300

    Improved tests

--- tests/RequestProcessor/AbstractStrategyTest.php
+++ tests/RequestProcessor/AbstractStrategyTest.php
@@ -3,6 +3,7 @@
 
 use Kami\Component\RequestProcessor\AbstractStrategy;
 use Kami\Component\RequestProcessor\Step\StepInterface;
+use Kami\Component\RequestProcessor\Step\AbstractStep;
 use PHPUnit\Framework\TestCase;
 
 class AbstractStrategyTest extends TestCase
@@ -14,13 +15,34 @@ class AbstractStrategyTest extends TestCase
 
     public function setUp()
     {
+        $step1 = $this->createMock(StepInterface::class);
+        $step2 = $this->createMock(StepInterface::class);
+
         $this->strategy = new class extends AbstractStrategy {
-            public static function getSteps(): array
+
+            /**
+             * @var array
+             */
+            protected static $testSteps = [];
+
+            /**
+             * @param array $steps
+             */
+            public static function addTestSteps(array $steps)
             {
-                return [];
+                static::$testSteps = $steps;
             }
 
+            /**
+             * @return array
+             */
+            public static function getSteps(): array
+            {
+                return static::$testSteps;
+            }
         };
+
+        $this->strategy::addTestSteps([$step1, $step2]);
     }
 
     public function testAddStep()
@@ -31,16 +53,19 @@ class AbstractStrategyTest extends TestCase
     }
 
 
-
     public function testGetNextStep()
     {
         $step1 = $this->createMock(StepInterface::class);
-        $step2 = $this->createMock(StepInterface::class);
+        $step2 = $this->createMock(AbstractStep::class);
 
         $this->strategy->addStep($step1);
         $this->strategy->addStep($step2);
 
+        $this->strategy->getNextStep();
+        $this->strategy->getNextStep();
+
         $this->assertEquals($step1, $this->strategy->getNextStep());
         $this->assertEquals($step2, $this->strategy->getNextStep());
+        $this->assertEquals(null, $this->strategy->getNextStep());
     }
 }
